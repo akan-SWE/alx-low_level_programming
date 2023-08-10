e67cfef64787e6168b893e6f12c2ffb66f2cd85b
akan-SWE <a.akanSWE@gmail.com>
2023-08-10 18:53:11 +0100

    Wrote the C program for 4-puts.c.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point for the program
+*
+* Description: print formated output to standard output
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
