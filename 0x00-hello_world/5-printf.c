624d756184ba539086e18b7602676ae11fd0323e
akan-SWE <a.akanSWE@gmail.com>
2023-08-10 19:20:12 +0100

    Wrote the C program for 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point for the program
+*
+* Descriptin: print formated output as standard output
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
