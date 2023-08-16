bb9a3fa52f4a4468177c493d6b895f9951f7b834
akan-SWE <a.akanSWE@gmail.com>
2023-08-16 10:25:58 +0100

    add: Wrote function prototypes that is needed in file 5-sign.c and 6-abs.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,8 +16,44 @@ int print_putchar(char *_putchar);
  */
 void print_alphabet(void);
 
+/**
+ * print_alphabet_x10 - print the alphabet in lowercase 'a' to 'z' 10 times
+ * Return: void
+ */
 void print_alphabet_x10(void);
 
+/**
+ * _islower - check for any lowercase alphabet
+ *
+ * @c: parameter type
+ *
+ * Return: '1' if true else '0'
+ */
 int _islower(int c);
 
+/**
+ * _isalpha - check for the argument is an alphabet
+ *
+ * @c: parameter type
+ *
+ * Return: '1' if true else '0'
+ */
 int _isalpha(int c);
+
+/**
+ * print_sign - print the sign of a number(negative, positive, zero)
+ *
+ * @n: parameter type
+ *
+ * Return: '1' if greater than zero, '0' is zero, '-1' is less than zero
+ */
+int print_sign(int n);
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @param: accespt a type of int
+ *
+ * Return: the absolute value
+ */
+int _abs(int);
