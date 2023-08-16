1d9113c4770ec274b7dcd5d46e3039a933e97d73
akan-SWE <a.akanSWE@gmail.com>
2023-08-16 19:33:09 +0100

    add: Wrote the source code for project file 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,49 +10,29 @@
  */
 void print_to_98(int n)
 {
-
 	if (n <= 98)
 	{
+
 		while (n <= 98)
 		{
-			if (n <= 9 && n >= 0)
-				_putchar(n + '0');
-			else if (n >= 10)
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			else if (n < 0 && n >= -9)
-			{
-				_putchar('-');
-				_putchar((n * -1) + '0');
-			}
+			printf("%d", n);
 
-			else if (n == -10)
-			{
-				_putchar('-');
-				_putchar((n / -10) + '0');
-				_putchar((n % -10) + '0');
-			}
 			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+				printf(", ");
 			n++;
 		}
+		printf("\n");
 	}
 	else
 	{
 		while (n >= 98)
 		{
 			printf("%d", n);
+
 			if (n != 98)
 				printf(", ");
-
 			n--;
 		}
+		printf("\n");
 	}
-
-	putchar('\n');
 }
