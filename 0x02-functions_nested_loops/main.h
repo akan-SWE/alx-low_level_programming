6b27b6643a14732f33aded45e6b50482370fc512
akan-SWE <a.akanSWE@gmail.com>
2023-08-16 05:07:51 +0100

    add: included function prototypes in the header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,21 @@
 #ifndef main_h
 #define main_h
-
+int _putchar(char c);
+#endif
 /*
  * print_putchar - print the sets of characters
  */
 int print_putchar(char *_putchar);
 
-#endif
+/**
+ *_putchar - Writes data to standard output.
+ *
+ * @character: Pointer to a character array containing data to be written.
+ *
+ * Return: The number of characters written (success)
+ */
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
