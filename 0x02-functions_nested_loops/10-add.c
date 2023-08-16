d3bbe017512f04c1bc9683b6da3085b695a013b4
akan-SWE <a.akanSWE@gmail.com>
2023-08-16 17:13:27 +0100

    add: Wrote the source code for project file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @first: first integer to be added
+ * @second: second integer to be added
+ *
+ * Return: result
+ */
+int add(int first, int second)
+{
+	int result;
+
+	result = first + second;
+	return (result);
+}
