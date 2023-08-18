0906f140bcfa3afbc233d047005a928e568ece01
akan-SWE <a.akanSWE@gmail.com>
2023-08-18 09:55:26 +0100

    add: Wrote the function of the project file 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: formal parameter that is used for checking
+ *
+ * Return: '1' if true and else '0'
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
