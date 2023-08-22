8635b69e1ce0ad2204804bd9f3a607a1492b4517
akan-SWE <a.akanSWE@gmail.com>
2023-08-22 02:10:02 +0100

    add: Implemented the function for project file 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of an integer to 98
+ *
+ * @n: - the integer pointer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n) { *n = 98; }
