be8ff6f675ae096ae9a262e9f7053beeea37eddc
akanSWE <a.akanSWE@gamil.com>
2023-09-07 20:29:43 +0100

    Add comments to improve code readability

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,16 +14,21 @@ int is_prime_number(int n)
 
 /**
  * prime_helper - checks if the nubmer is a prime number
+ *
  * @n: the number to check
  * @check: used to check if the number are prime
+ *
  * Return: 1 if it a prime number and 0 otherwise
  */
 int prime_helper(int n, int check)
 {
+	/* if n is less than or equal to 1 it not a prime number */
 	if (n <= 1)
 		return (0);
+	/* if the closest square is greater than n, it means n is a prime */
 	if (check * check > n)
 		return (1);
+	/* if n is divisible by check it is not a prime number*/
 	if (n % check == 0)
 		return (0);
 
