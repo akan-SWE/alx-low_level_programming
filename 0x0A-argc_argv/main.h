40c99d3cc3e1a2322ca14082ff26e15bc504567c
akanSWE <a.akanSWE@gamil.com>
2023-09-18 00:03:26 +0100

    Add function prototypes and system headers in the header file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*system headers*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*Function prototypes*/
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of argument passed
+ * @argv: the argument
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[]);
+
+#endif /*MAIN_H*/
