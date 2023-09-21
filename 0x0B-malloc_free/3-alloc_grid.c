345f32b44862522e87d0a549fb2c31adb039bc93
akanSWE <a.akanSWE@gamil.com>
2023-09-21 17:03:25 +0100

    Add: Implemented function alloc_grid in the project file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * alloc_grid - creates a 2 dimentional array
+ *
+ * @width: the width or number of rows
+ * @height: the height or number of columns
+ *
+ * Return:
+ * - A pointer to the 2 dimentional array
+ * - NULL on failure
+ * - NULL if @width or @height is 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, k, j, **grid_array;
+
+	/* Create pointers of the fixed height */
+	grid_array = (int **)malloc(height * sizeof(int *));
+
+	/* Memory allocation failed, hence return */
+	if (grid_array == NULL)
+	{
+		write(1, "failed memory allocation", STRING_SIZE);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+	/* The pointers created, will point, to a 1D array each */
+		grid_array[i] = (int *)malloc(width * sizeof(int));
+    
+    /*memory allocating failed for each 1D array so free and return*/
+    if (grid_array[i] == NULL)
+    {
+      for (k = 0; k < i; k++)
+        free(grid_array[k]);
+      free(grid_array);
+      return (NULL);
+    }
+	}
+
+
+	i = 0;
+	/* For every heighth, width interactes with a value of 0*/
+	while (i < height)
+	{
+		j = 0;
+		while (j < width)
+		{
+			grid_array[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+
+	return (grid_array); /* The grid*/
+}
+
