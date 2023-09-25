fec3afcfefdfece1d75f65fa9e3607af1330332d
akanSWE <a.akanSWE@gamil.com>
2023-09-25 14:39:30 +0100

    Update: Changed the formal parameter from str to s1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,32 +2,32 @@
 
 /**
  * _strcmp - compare two string together
- * @str1: first string
+ * @s1: first string
  * @s2: second string
  *
  * Return: result
  */
 
-int _strcmp(char *str1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int result, i;
 
 	i = 0;
-	while (str1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (str1[i] == s2[i])
+		if (s1[i] == s2[i])
 		{
 			result = 0;
 			i++;
 		}
-		else if (str1[i] > s2[i])
+		else if (s1[i] > s2[i])
 		{
-			result = str1[i] - s2[i];
+			result = s1[i] - s2[i];
 			break;
 		}
-		else if (str1[i] < s2[i])
+		else if (s1[i] < s2[i])
 		{
-			result = str1[i] - s2[i];
+			result = s1[i] - s2[i];
 			break;
 		}
 	}
