367969466340c156ca52306952ef98446aec8dcf
akanSWE <a.akanSWE@gamil.com>
2023-09-25 14:19:01 +0100

    Update: no changes added just formatted the project file

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
-* string_toupper - convert lowercase string to uppercase
-*
-* @s: the string to be converted
-*
-* Return: the converted string
-*/
+ * string_toupper - convert lowercase string to uppercase
+ *
+ * @s: the string to be converted
+ *
+ * Return: the converted string
+ */
 
 char *string_toupper(char *s)
 {
 	int i, common_difference;
 
 	char *convert = s; /*pointer to the string*/
-	
+
 	common_difference = 32;
 	/*convert the string to uppercase*/
 	for (i = 0; s[i] != '\0'; i++)
