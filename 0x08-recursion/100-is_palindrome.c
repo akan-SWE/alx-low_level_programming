0a95b34448040010354d61272fd07c0714e377e5
akanSWE <a.akanSWE@gamil.com>
2023-09-28 18:29:55 +0100

    add: Implemented the functions that checks for palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if string is a palindrome.
+ *
+ * The function checks if string is palindrome by utilizing
+ * the rev_and_compare function.
+ *
+ * @s: The input string to check for palindrome.
+ *
+ * Return: the result of the rev_and_compare function
+ * (1 if palindrome, 0 otherwise)
+ */
+int is_palindrome(char *s)
+{
+	int i;
+	char *clone = NULL;
+
+	/* Need a clone so that it remains constant during recursion */
+	clone = s;
+
+	return (rev_and_compare(s, clone, i, strlen(s)));
+}
+
+/**
+ * rev_and_compare - Reverses the string and compare.
+ *
+ * This function reverses a string and compares it will it original string
+ * as long as it remains the same it returns 1, 0 otherwise.
+ *
+ * @s: The string to reverse and compare.
+ * @clone: a copy of the original string to prevent changes during recursion.
+ * @i: Current index.
+ * @length: The length of the string.
+ *
+ * Return: 1 if it a palindrome and 0 otherwise.
+ */
+int rev_and_compare(char *s, char *clone, int i, int length)
+{
+	if (*s == '\0')
+		return (1);
+
+	if (clone[length - 1 - i] == clone[i])
+		return (rev_and_compare(s + 1, clone, i++, length));
+	else
+		return (0);
+}
+
