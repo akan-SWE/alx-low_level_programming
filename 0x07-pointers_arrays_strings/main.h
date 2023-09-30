0bb5b72e757d797a5b834a527b82d1e8f53231a9
akanSWE <a.akanSWE@gamil.com>
2023-09-30 19:46:22 +0100

    add: Function prototype for memset in the header file

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _memset - fill memory with a constant character
+ *
+ * @s: destination string
+ * @b: the character to fill with
+ * @n: number of blocks to fill
+ *
+ * Return: a pointer of the string
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
