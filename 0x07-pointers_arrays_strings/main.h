ddac16af76a5cd34f94b2a51e3d6ec2db46f3a42
akanSWE <a.akanSWE@gamil.com>
2023-09-30 19:52:20 +0100

    add: Function prototype of memcpy and strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* system header */
+#include <stdlib.h>
+
 /**
  * _memset - fill memory with a constant character
  *
@@ -11,5 +14,27 @@
  * Return: a pointer of the string
  */
 char *_memset(char *s, char b, unsigned int n);
+
+/**
+ * _memcpy - copy memory area;
+ *
+ * @dest: destination string to copy to
+ * @src: string to be copied to destination string
+ * @n: number of characters to copy
+ *
+ * Return: a pointer, dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/**
+ * _strchr - checks for any occurence of c in the string
+ * and return a pointer to the first occurrence
+ *
+ * @s: the string
+ * @c: the character to check for in the string
+ *
+ * Return: a pointer to the first occurence
+ */
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
