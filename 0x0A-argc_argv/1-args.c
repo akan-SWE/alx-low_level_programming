298bf85197e3542dfc5b41a360262a5efe51160a
akanSWE <a.akanSWE@gamil.com>
2023-10-02 19:47:58 +0100

    add: Defined the function that prints the number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,28 @@
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: array of strings or pointers
+ *
+ * Return: 0 (Success)
+ */
+#include "main.h"
+#include <stdio.h>
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	print_arg_count(argc, argv);
+	return (0);
+}
+/**
+ * print_arg_count - prints number of argument
+ *
+ * @argc: number of arguments passed
+ * @argv: the argument
+ *
+ * Return: void (Nothing)
+ */
+void print_arg_count(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc);
+}
