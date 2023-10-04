1657d5d61753c800e5ae79bed644e8853477b98e
akanSWE <a.akanSWE@gamil.com>
2023-10-04 02:54:21 +0100

    add: Implemented the function create_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of char
+ * and initialize it with a specific value
+ *
+ * @size: size of the array
+ * @c: character to initialize to
+ *
+ * Return:
+ *  - NULL if @size = 0
+ *  - A pointer to the array
+ *  - NULL if it failes
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+
+	if (size == 0)
+		return (NULL);
+
+	char *pArray = malloc(size * sizeof(char));
+
+	/* Memory allocation failed */
+	if (pArray == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < size)
+		pArray[i++] = c;
+
+	return (pArray);
+}
+
