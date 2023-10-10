82714de187b223ab802ce7dc39a942761a269e5d
akanSWE <a.akanSWE@gamil.com>
2023-10-10 21:56:11 +0100

    add: Implemented the function print_dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ *
+ * @d: The object pointer
+ *
+ * Return: void (Nothing)
+ */
+void print_dog(struct dog *d)
+{
+
+	/* Name */
+	if (d->name == NULL)
+		printf("nil\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	/* Age */
+	if (isnan(d->age))
+		printf("nil\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	/* Owner */
+	if (d->owner == NULL)
+		printf("nil\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
