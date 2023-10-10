729213a56bdd9b7a76cde0c8ee091ff33eed6612
akanSWE <a.akanSWE@gamil.com>
2023-10-10 21:25:04 +0100

    add: Function prototype init_dog and it description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,4 +19,16 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - initializes a variable of type struct dog
+ *
+ * @d: The dog object pointer of type struct dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Return: void (Nothing)
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif /* DOG_H */
