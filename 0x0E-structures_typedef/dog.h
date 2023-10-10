b6d16733f8d74e8f1a973cffec12f9ef074a4994
akanSWE <a.akanSWE@gamil.com>
2023-10-11 00:18:24 +0100

    add: Function prototype of new_dog and descriptions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -38,5 +38,24 @@ typedef struct dog dog_t;
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
+/**
+ * print_dog - prints a struct dog
+ *
+ * @d: The object pointer
+ *
+ * Return: void (Nothing)
+ */
 void print_dog(struct dog *d);
+
+/**
+ * new_dog - Creates a new dog
+ *
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Return: The new dog that is created, otherwise NULL if the function
+ * fails
+ */
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /* DOG_H */
