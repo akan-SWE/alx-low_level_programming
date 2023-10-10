4d3bebec97684a38f15b25491a6d5ec270a469bf
akanSWE <a.akanSWE@gamil.com>
2023-10-10 22:00:22 +0100

    update: Implementation to match the output requirement when a member of d, is null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,19 +12,19 @@ void print_dog(struct dog *d)
 
 	/* Name */
 	if (d->name == NULL)
-		printf("(nil)\n");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
 	/* Age */
 	if (isnan(d->age))
-		printf("(nil)\n");
+		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 
 	/* Owner */
 	if (d->owner == NULL)
-		printf("(nil)\n");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
