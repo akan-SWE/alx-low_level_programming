5c63968cfe279e80f41ca2f3987a43be2a16dd96
akanSWE <a.akanSWE@gamil.com>
2023-10-11 11:32:26 +0100

    fix: 97 Segmentation fault

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * print_dog - prints a struct dog
@@ -10,21 +11,10 @@
 void print_dog(struct dog *d)
 {
 
-	/* Name */
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-
-	/* Age */
-	if (isnan(d->age))
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
-
-	/* Owner */
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	}
 }
