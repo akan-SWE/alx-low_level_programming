306964f1b90688abdd62a10a0c877f34340a2915
akanSWE <a.akanSWE@gamil.com>
2023-10-12 14:39:07 +0100

    update: Header file with descriptions of the function prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -16,8 +16,30 @@
  */
 void print_name(char *name, void (*f)(char *));
 
+/**
+ * array_iterator - Executes a function given as a parameter on each element
+ * of an array
+ *
+ * @array: The array
+ * @size: Size of the array
+ * @action: The function pointer that points the function passed
+ *
+ * Return: void (Nothing)
+ */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+/**
+ * int_index - searches for an integer
+ *
+ * @array: The array
+ * @size: Size of the array
+ * @cmp: The function pointer
+ *
+ * Return:
+ * - The index when @cmp returns 1, otherwise
+ * - -1 if cmp or array == NULL, or size is less than or equals to 0
+ * - -1 if no match
+ */
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
