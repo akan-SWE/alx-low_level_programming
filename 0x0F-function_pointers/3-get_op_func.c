1f4ad0bf151745bfa472621e7bd1131458583690
akanSWE <a.akanSWE@gamil.com>
2023-10-12 18:00:27 +0100

    fix: error on check 17

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op)
 	{
-		if (*s == *ops[i].op && (*ops[i].op != '\0'))
+		if (*s == *ops[i].op && !s[1])
 			return (ops[i].f);
 		i++;
 	}
