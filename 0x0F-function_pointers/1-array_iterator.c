4ac227a2fc2968c2bbef9743905ceedc66e7a001
akanSWE <a.akanSWE@gamil.com>
2023-10-12 14:21:14 +0100

    update: handled the case where array is null or size is 0

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned long int index;
 
 	/* The pointer function is null or empty */
-	if (action == NULL)
+	if (action == NULL || array == NULL || size == 0)
 		return;
 
 	for (index = 0; index < size; index++)
