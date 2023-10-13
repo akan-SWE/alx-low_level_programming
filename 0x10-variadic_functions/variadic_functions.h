5a5a5ae7fc5c137596eaea61d37c0166f217ffca
akanSWE <a.akanSWE@gamil.com>
2023-10-13 14:12:33 +0100

    add: String alias to char *

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef char *String;
+
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
