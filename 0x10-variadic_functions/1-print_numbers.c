2e68fae6d69ab7dbfce2b5a5b9f534d3921ecc9b
akanSWE <a.akanSWE@gamil.com>
2023-10-13 13:49:27 +0100

    add: Implemented the function print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+
+/**
+ * print_numbers - prints numbers
+ *
+ * @separator: The separator for each number
+ * @n: The number of argmument to collect
+ *
+ * Return: void (Nothing)
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	size_t i;
+	va_list args;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+
+		printf("%d", va_arg(args, int));
+
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+	}
+	putchar('\n');
+
+	va_end(args);
+}
