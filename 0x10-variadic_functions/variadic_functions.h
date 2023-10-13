f10904729e53238085c255740beecb6e85150463
akanSWE <a.akanSWE@gamil.com>
2023-10-13 15:37:43 +0100

    add: structure and function prototypes for task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -17,5 +17,25 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
+/**
+ * struct specifierHandler - a structure that maps the format specifier
+ * to the corresponding function
+ *
+ * @format: The key of character
+ * @handler_ptr: The value or function to call
+ */
+typedef struct specifierHandler
+{
+	char format;
+	void (*handler_ptr)(va_list args);
+} s_h;
+
+void print_string(va_list args);
+
+void print_integer(va_list args);
+
+void print_float(va_list args);
+
+void print_chars(va_list args);
 #endif /* VARIADIC_FUNCTION_H */
 
