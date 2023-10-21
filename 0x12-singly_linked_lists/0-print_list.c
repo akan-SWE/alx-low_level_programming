b2e612c8aeaf8b72155e46454c68026f81915728
akanSWE <a.akanSWE@gamil.com>
2023-10-21 12:06:38 +0100

    fix: the output when str is null

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL) /* element is null */
-			printf("[] (nil)\n");
+			printf("[0] (nil)\n");
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
