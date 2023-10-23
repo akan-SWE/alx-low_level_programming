30db2e21b0b05ccf7c07cf670aebb1bffe871764
akanSWE <a.akanSWE@gamil.com>
2023-10-23 18:07:17 +0100

    add: Implemented the function print_listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_lisint - prints all element of a @listint_t list
+ *
+ * @h: A pointer to the first node in the list
+ *
+ * Return: Number of nodes in the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	int noOfNodes;
+
+	noOfNodes = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+
+		h = h->next;
+		noOfNodes++;
+	}
+
+	return (noOfNodes);
+}
