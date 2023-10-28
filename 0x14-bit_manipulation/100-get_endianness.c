5a14b14d4810d1a6393d947cfb7e6d15276915a9
akanSWE <a.akanSWE@gamil.com>
2023-10-28 19:10:22 +0100

    add: Wrote the function get_endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness
+ *
+ * Return: 1 if it a little endian, and 0 if it a big endian
+ */
+int get_endianness(void)
+{
+	int num, i;
+
+	num = 1;
+	if (*(unsigned char *)&num == 1)
+		return (1); /* Little endian */
+	else
+		return (0); /* Big endian */
+}
