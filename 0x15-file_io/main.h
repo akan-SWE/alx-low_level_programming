3bf833ebf911f81e686e9cdd60ba9e1021e97e20
akanSWE <a.akanSWE@gamil.com>
2023-10-31 18:45:28 +0100

    add: Function prototype create_file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -19,6 +19,16 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters);
 
+/**
+ * create_file - Creates a file
+ * and insert text to it.
+ *
+ * @filename: The name of the file.
+ * @text_content: The text to write to it.
+ *
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…).
+ */
 int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
