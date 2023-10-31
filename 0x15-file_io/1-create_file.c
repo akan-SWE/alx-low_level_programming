2e01ce8a3e1f6361406905c5f9f94385e55af5bd
akanSWE <a.akanSWE@gamil.com>
2023-10-31 18:43:36 +0100

    add: Implemented the function create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * and insert text to it.
+ *
+ * @filename: The name of the file.
+ * @text_content: The text to write to it.
+ *
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…).
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	/* Create file */
+	fd = open(filename,  O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	/* Failed to create file */
+	if (fd == -1)
+	{
+		perror("File cannot be created");
+		close(fd);
+		return (-1);
+	}
+
+	if (write(fd, text_content, strlen(text_content)) == -1)
+	{
+		perror("File cannot be written to");
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1); /* Success*/
+}
