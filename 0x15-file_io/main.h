af2d0631626d7bce4e54bcfe75baa262544e0839
akanSWE <a.akanSWE@gamil.com>
2023-10-31 18:06:41 +0100

    add: more header files and description for read_textfile function

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,19 @@
 /* System headers */
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
+/**
+ * read_textfile - reads a text file and prints it to the
+ * POSIX standard output.
+ *
+ * @filename: The name of the file
+ * @letters: Number of characters to read
+ *
+ * Return: Number of characters written, otherwise 0 if the function fails
+ */
 ssize_t read_textfile(const char *filename, size_t letters);
 
 int create_file(const char *filename, char *text_content);
