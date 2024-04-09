63e8ce9cba32c9cb2e8d7f1682c92cdb27d75688
akanSWE <a.akanSWE@gamil.com>
2024-04-09 21:56:35 +0100

    refactor(2-key_index.c): used bitwise operator for the calculation instead

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -13,5 +13,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	/* generate the hash value */
 	unsigned long int hash_value = hash_djb2(key);
 	/* return the index based on the size of the array */
-	return (hash_value % size);
+	return (hash_value & (size - 1));
 }
